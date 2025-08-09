8f267850bca92eee29c933ea6061cdee0223caed
Abulifa <abulifa@outlook.com>
2025-08-09 23:28:27 +0200

    fix padding

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -10,7 +10,9 @@ const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
   return (
     <div className="w-full h-screen flex flex-col">
       <Navbar />
-      <div className="flex-1 px-10 lg:px-32 py-3 bg-accent">{children}</div>
+      <div className="flex-1 px-4 sm:px-6 md:px-10 lg:px-32 py-3 bg-accent">
+        {children}
+      </div>
     </div>
   );
 };
